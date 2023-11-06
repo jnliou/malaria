1384a0076411ce0ab4ee815c1abbc563d881f159
jnliou <julialiou@alumni.ubc.ca>
2023-11-06 00:39:01 -0500

    ml changes

--- assets/js/ml-model.js
+++ assets/js/ml-model.js
@@ -24,3 +24,29 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+// Xception
+
+// Xception
+var modal3 = document.getElementById('xceptionModal');
+// Get the button that opens the modal
+var btn3 = document.querySelector('.modal-trigger3'); // Changed variable name to btn3 to avoid conflicts
+// Get the <span> element that closes the modal
+var span3 = document.getElementsByClassName("close3")[0]; // Changed variable name to span3 to avoid conflicts
+
+// When the user clicks on the button, open the modal
+btn3.onclick = function() {
+    modal3.style.display = "block";  // Corrected to modal3
+}
+
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+    modal3.style.display = "none";  // Corrected to modal3
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal3) {  // Corrected to modal3
+        modal3.style.display = "none";
+    }
+}
+
