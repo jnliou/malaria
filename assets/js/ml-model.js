3dc69531e7888f296f9902447e4f1463def429ce
jnliou <julialiou@alumni.ubc.ca>
2023-11-06 13:00:07 -0500

    changes to ML model section

--- assets/js/ml-model.js
+++ assets/js/ml-model.js
@@ -1,32 +1,60 @@
 // Popup for machine learning model
+
+
+//CNN
 // Get the modal
-var modal = document.getElementById("randomForestModal");
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("modal-trigger");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn[0].onclick = function() {
-  modal.style.display = "block";
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Random Forest
+var modal2 = document.getElementById("randomForestModal");
+
+// Get the button that opens the modal
+var btn2 = document.getElementsByClassName("modal-trigger2");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on the button, open the modal
+btn2[0].onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event2) {
+  if (event2.target == modal2) {
+    modal2.style.display = "none";
   }
 }
 // Xception
 
-// Xception
 var modal3 = document.getElementById('xceptionModal');
 // Get the button that opens the modal
 var btn3 = document.querySelector('.modal-trigger3'); // Changed variable name to btn3 to avoid conflicts
@@ -44,9 +72,10 @@ span3.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal3) {  // Corrected to modal3
+window.onclick = function(event3) {
+    if (event3.target == modal3) {  // Corrected to modal3
         modal3.style.display = "none";
     }
 }
+
 
